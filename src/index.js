b2baeff821fe5cedd6ab0623dc407f16aab845e2
ssbien <106289650+ssbien@users.noreply.github.com>
2022-09-12 18:58:59 -0800

    coded forecast prediction in js, changed bg in css, removed fahrenheit

--- src/index.js
+++ src/index.js
@@ -25,9 +25,67 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 let today = document.querySelector("#todays-date");
 
 today.innerHTML = formatDate(now);
+
+//
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Mon", "Tue", "Wed", "Thu"];
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
+  <div class="col">
+  <div class="forecast-col">
+    <div class="forecast-temperature">
+      <span class="forecast-temperature-min">
+        ${Math.round(forecastDay.temp.min)}°
+      </span>
+      /
+      <span class="forecast-temperature-max">
+        ${Math.round(forecastDay.temp.max)}°
+      </span>
+    </div>
+    <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+    />
+    <div class="forecast-date">
+        ${formatDay(forecastDay.dt)}
+    </div>
+  </div>
+</div>
+`;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "a710bd8bd76400c9658ef649d9e81728";
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(displayForecast);
+}
 
 //
 
@@ -50,6 +108,8 @@ function displayWeather(response) {
 
   document.querySelector("#weather-type").innerHTML =
     response.data.weather[0].main;
+
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
@@ -82,7 +142,7 @@ form.addEventListener("submit", submit);
 
 searchCity("New York");
 
-// C to F
+// C to F - not using at the moment but will work on integration including forecast temps and main temp
 let celsiusTemperature = null;
 
 function convertToFahrenheit(event) {
